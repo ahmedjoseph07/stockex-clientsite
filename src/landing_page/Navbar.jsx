056cd2925ab0617545ddb2f90e238b6a8b59ee20
Joseph Ahmed <94310209+ahmedjoseph07@users.noreply.github.com>
2024-11-12 01:48:24 +0600

    minor bug fixes

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -121,7 +121,7 @@ const Navbar = () => {
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             { isLoggedIn ? (
-                <Link to="http://stockex-dashboard.vercel.app">Dashboard</Link>
+                <Link to="https://stockex-dashboard.vercel.app">Dashboard</Link>
             ) : (
                 <span>Dashboard (Login Required)</span>
             )}
