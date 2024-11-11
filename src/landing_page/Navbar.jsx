5910213e2ac4c5e765bc0baf5a70b6516d8e0134
Joseph Ahmed <94310209+ahmedjoseph07@users.noreply.github.com>
2024-11-12 01:45:03 +0600

    signup bug fix

--- src/landing_page/Navbar.jsx
+++ src/landing_page/Navbar.jsx
@@ -194,7 +194,7 @@ const Navbar = () => {
                 </div>
                 <button type="submit" className="w-full text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Login</button>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                  Not registered? <a href="https://stockex-clientsite.vercel.app/signup" className="text-green-700 hover:underline dark:text-green-500">Create account</a>
+                  Not registered? <Link to="/signup" className="text-green-700 hover:underline dark:text-green-500">Create account</Link>
                 </div>
               </form>
             </div>
